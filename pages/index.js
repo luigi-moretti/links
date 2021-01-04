86e9386e097ea527cda2bceb824eba17b1826516
Luigi Moretti <luigi_morettii@hotmail.com>
2021-01-04 05:41:49 -0300

    Ajustes iniciais

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,73 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Grid, Button, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import {ThemeProvider} from '@material-ui/core';
+import Head from 'next/head';
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    background: "rgb(63,251,152)",
+    background: "radial-gradient(circle, rgba(63,251,152,1) 0%, rgba(19,75,80,1) 80%)", 
+    //background: "linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%)",
+    height: "100vh"
+  },
+  fullHeight:{
+    height:"100%"
+  },
+  image: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+  
+
+}));
 
 export default function Home() {
+  const classes = useStyles();
+  
   return (
-    <div className={styles.container}>
+    <div className={classes.root}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Links</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        className={classes.fullHeight}
+      >
+        <Grid item container direction="column" spacing={2} xs={10} sm={4}>
+            <Grid item>
+              <img className={classes.image} alt="perfil" src="/vercel.svg" />
+            </Grid>
+            <Grid>
+              <Typography align="center">
+                Luigi Moretti
+              </Typography>
+            </Grid>
+            <Grid item xs>
+              <Button variant="contained" color="primary" fullWidth>
+                Linkedin
+              </Button>
+            </Grid>
+            <Grid item xs>
+              <Button variant="contained" color="primary" fullWidth>
+                Linkedin
+              </Button>
+            </Grid>
+            <Grid item xs>
+              <Button variant="contained" color="primary" fullWidth>
+                Linkedin
+              </Button>
+            </Grid>
+        </Grid>
+      </Grid>
     </div>
   )
 }
